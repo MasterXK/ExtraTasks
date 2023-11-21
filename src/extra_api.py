939add78bef284d290118050beca60c8e89df17e
vavil <vavilon164@yandex.ru>
2023-11-21 19:51:15 +0300

    Added 1-3 tasks from logging

--- src/extra_api.py
+++ src/extra_api.py
@@ -1,12 +1,28 @@
 import os
-
+import logging
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
+from data import PATH_DATA
 
 load_dotenv()
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def get_transaction_sum(transaction: dict) -> float:
+file_handler = logging.FileHandler(os.path.join(PATH_DATA, 'api_log.log'), 'w')
+file_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def get_transaction_sum(transaction: dict) -> float | None:
     """
     Функция выводит транзакцию в рублях
     :param transaction: транзакция
@@ -15,34 +31,59 @@ def get_transaction_sum(transaction: dict) -> float:
     target_currency = transaction["operationAmount"]["currency"]["code"]
 
     if target_currency == "RUB":
+        logger.info('Транзакция в рублях, вывожу сумму...')
         return float(transaction["operationAmount"]["amount"])
 
     else:
+        logger.info(f'Транзакция в валюте {target_currency}, запрашиваю курс к рублю...')
         key = os.getenv("API_EXC")
-        response = requests.get(
-            f"https://v6.exchangerate-api.com/v6/{key}/latest/{target_currency}"
-        )
-        data = response.json()
 
-        return float(
+        try:
+            response = requests.get(
+                f"https://v6.exchangerate-api.com/v6/{key}/latest/{target_currency}"
+            )
+            data = response.json()
+            logger.debug('Данные курса получены.')
+
+        except requests.exceptions.RequestException as e:
+            logger.debug(f'Не удалось получить данные: {e}', exc_info=True)
+            return None
+
+        logger.info('Курс валюты получен, вывожу сумму транзакции...')
+        return round(
             data["conversion_rates"]["RUB"]
-            * float(transaction["operationAmount"]["amount"])
+            * float(transaction["operationAmount"]["amount"]),
+            2,
         )
 
 
-print(
-    get_transaction_sum(
-        {
-            "id": 41428829,
-            "state": "EXECUTED",
-            "date": "2019-07-03T18:35:29.512364",
-            "operationAmount": {
-                "amount": "100",
-                "currency": {"name": "USD", "code": "USD"},
-            },
-            "description": "Перевод организации",
-            "from": "MasterCard 7158300734726758",
-            "to": "Счет 35383033474447895560",
-        }
-    )
-)
+def log_messages(level: str, message: str) -> None:
+    time_now = datetime.now()
+    today = time_now.strftime('%Y-%m-%d.log')
+    file_path = os.path.join(PATH_DATA, 'logs', today)
+
+    sh = logging.StreamHandler()
+    fh = logging.FileHandler(file_path)
+    logging.basicConfig(handlers=(fh, sh),
+                        level=getattr(logging, level.upper()),
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    if level.upper() == 'DEBUG':
+        logging.debug(message)
+
+    elif level.upper() == 'INFO':
+        logging.info(message)
+
+    elif level.upper() == 'WARNING':
+        logging.warning(message)
+
+    elif level.upper() == 'ERROR':
+        logging.error(message)
+
+    else:
+        logging.critical(message)
+
+
+log_messages("info", "Это информационное сообщение.")
+log_messages("warning", "Это предупреждение.")
+log_messages("error", "Это ошибка.")
